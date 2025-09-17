595a96be0ca8d8a96cfcbca63c42b9b2073a2736
sauverma93 <saurabh.verma@amd.com>
2025-09-17 08:59:24 -0500

    fix formatting issues

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/pc_sampling/ioctl/ioctl_adapter.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/pc_sampling/ioctl/ioctl_adapter.cpp
@@ -469,7 +469,7 @@ convert_ioctl_pcs_config_to_rocp(const rocprofiler_ioctl_pc_sampling_info_t& ioc
 
 rocprofiler_status_t
 check_firmware_compatibility(const rocprofiler_agent_t*       agent,
-                                        rocprofiler_pc_sampling_method_t method)
+                             rocprofiler_pc_sampling_method_t method)
 {
     if(!agent) return ROCPROFILER_STATUS_ERROR_AGENT_NOT_FOUND;
 
@@ -561,7 +561,8 @@ ioctl_query_pcs_configs(const rocprofiler_agent_t* agent, rocp_pcs_cfgs_vec_t& r
             // This should never happened, unless the KFD is broken.
             continue;
         }
-        if(check_firmware_compatibility(agent, get_rocp_pcs_method_from_kfd(ioctl_cfg.method)) == ROCPROFILER_STATUS_SUCCESS)
+        if(check_firmware_compatibility(agent, get_rocp_pcs_method_from_kfd(ioctl_cfg.method)) ==
+           ROCPROFILER_STATUS_SUCCESS)
             rocp_configs.emplace_back(rocp_cfg);
     }
 
@@ -623,11 +624,11 @@ ioctl_pcs_create(const rocprofiler_agent_t*       agent,
                  uint64_t                         interval,
                  uint32_t*                        ioctl_pcs_id)
 {
-    if(check_firmware_compatibility(agent, method) != ROCPROFILER_STATUS_SUCCESS)
-        return ROCPROFILER_STATUS_INCOMPATIBLE_FIRMWARE;
+    auto status = check_firmware_compatibility(agent, method);
+    if(status != ROCPROFILER_STATUS_SUCCESS) return status;
 
     pcs_ioctl_version_t pcs_ioctl_version = 0;
-    auto status = get_pcs_ioctl_version_if_kfd_supports(agent->gpu_id, &pcs_ioctl_version);
+    status = get_pcs_ioctl_version_if_kfd_supports(agent->gpu_id, &pcs_ioctl_version);
     if(status != ROCPROFILER_STATUS_SUCCESS) return status;
 
     // Strict check: whether the driver version (safely) supports the sampling method for
