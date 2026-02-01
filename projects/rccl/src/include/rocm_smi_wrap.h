3f31d17ae70291df46f90e0219cf482aa3f7fe2d
Donato Capitella <donato.capitella@reversec.com>
2026-02-01 11:44:38 +0000

    build: Add compile-time error when ROCM_SMI is disabled.

--- projects/rccl/src/include/rocm_smi_wrap.h
+++ projects/rccl/src/include/rocm_smi_wrap.h
@@ -36,6 +36,7 @@ ncclResult_t rocm_smi_getDevicePciBusIdString(uint32_t deviceIndex, char* pciBus
 ncclResult_t rocm_smi_getDeviceIndexByPciBusId(const char* pciBusId, uint32_t* deviceIndex);
 ncclResult_t rocm_smi_getLinkInfo(int srcDev, int dstDev, RSMI_IO_LINK_TYPE* rsmi_type, int *hops, int *count);
 #else
+#error "DEBUG_TRACE: SMI is indeed disabled, using inline functions."
 inline ncclResult_t rocm_smi_init() { return ncclSuccess; }
 inline ncclResult_t rocm_smi_getNumDevice(uint32_t* num_devs) { *num_devs = 0; return ncclSuccess; }
 inline ncclResult_t rocm_smi_getDevicePciBusIdString(uint32_t deviceIndex, char* pciBusId, size_t len) { if (len > 0) pciBusId[0] = '\0'; return ncclSuccess; }
