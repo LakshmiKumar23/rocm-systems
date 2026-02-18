f4e06104fdd43de98e420721f5eaf935bc63681f
bgopesh <gopesh.bhardwaj@amd.com>
2026-02-18 11:34:04 +0530

    updating documentation for --selected-regions

--- projects/rocprofiler-sdk/source/bin/rocprofv3.py
+++ projects/rocprofiler-sdk/source/bin/rocprofv3.py
@@ -576,7 +576,7 @@ For attachment profiling of running processes:
     add_parser_bool_argument(
         filter_options,
         "--selected-regions",
-        help="If set, rocprofv3 will only profile regions of code surrounded by roctxProfilerResume(0) and roctxProfilerPause(0)",
+        help="If set, rocprofv3 will only profile regions of code surrounded by roctxProfilerResume(0) and roctxProfilerPause(0). Needs to be combined with `--marker-trace` option",
     )
 
     perfetto_options = parser.add_argument_group("Perfetto-specific options")
